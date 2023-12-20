4a12f0a32bfcffc39fdcab69601a66a6b9dcfa46
abhijit2712 <74667882+abhijit2712@users.noreply.github.com>
2023-12-20 13:19:45 +0530

    Create 12 | 21. Merge Two Sorted Lists.cpp

---
+++ LinkedList/Problems/12 | 21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,28 @@
+Approach 1 : Iterative approach
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(1);
+        ListNode* curr = dummy;
+
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                curr->next =list1;
+                list1 = list1->next;
+            }else{
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            if(curr->next != NULL) curr = curr->next;
+        }
+
+        if(list1) curr->next = list1;
+        if(list2) curr->next = list2;
+
+        return dummy->next;
+    }
+};
+
+
+
+Approach 2:
