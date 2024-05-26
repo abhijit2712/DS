8b52fdc62babf096404ebb22854ed903345b7354
abhijit2712 <74667882+abhijit2712@users.noreply.github.com>
2024-05-26 12:16:06 +0530

    Create 48. Rotate Image.cpp

---
+++ Sheet/Day-2/48. Rotate Image.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dummy(n, vector<int>(n,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dummy[i][j] = matrix[j][i];
+            }
+            reverse(dummy[i].begin(),dummy[i].end());
+        }
+
+        matrix = dummy;
+    }
+};
+
+
+// Approach 2 :
+class Solution {
+public:
+    void rotate(vector<vector<int>>& arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+        vector<vector<int>> ans(n,vector<int> (m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[i][j] = arr[n-j-1][i];
+            }
+        }
+        arr = ans;
+    }
+};
